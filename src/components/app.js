ea65f51d19368dc001ccdd2df5d0e578b86abd8f
Shawn Levin <shawnlevin22@gmail.com>
2018-01-29 14:41:48 -0800

    started proto

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,11 @@
+import React from 'react';
+import './app.css';
 
+export default () => {
+    return (
+        <div className="main-container">
+            <h1 className="main-title">Scrapbook</h1>
+        </div>
+    )
+}
 
